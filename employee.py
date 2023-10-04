f1ec7329c8b66424989c6658128cc2d8a086d954
Yasmin Alzaid <92607282+YasmineX@users.noreply.github.com>
2023-10-04 12:14:14 +0100

    Update employee.py
    Completed exercise

--- employee.py
+++ employee.py
@@ -2,7 +2,7 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 class Employee:
-    def __init__(self, name, salary, hours, hourly_rate, contracts, rate, bonus):
+    def __init__(self, name, salary=0, hours=0, hourly_rate=0, contracts=0, rate=0, bonus=0):
         self.name = name
         self.salary = salary
         self.hours = hours
